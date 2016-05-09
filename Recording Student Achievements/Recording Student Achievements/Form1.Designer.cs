49e31a23256d2c413874ed9b5f4d1b21d78fc4ca
Alex <Alex@ALEXHILL4CD8>
2016-05-09 15:51:13 +1200

    Added window sizes

--- Recording Student Achievements/Recording Student Achievements/Form1.Designer.cs
+++ Recording Student Achievements/Recording Student Achievements/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Recording_Student_Achievements
+﻿using System.Windows.Forms;
+
+namespace Recording_Student_Achievements
 {
     partial class Form1
     {
@@ -28,12 +30,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1370, 104);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1904, 1121);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Recording Student Achievements";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            this.MinimumSize = Screen.PrimaryScreen.WorkingArea.Size;
+            this.MaximumSize = Screen.PrimaryScreen.WorkingArea.Size;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
