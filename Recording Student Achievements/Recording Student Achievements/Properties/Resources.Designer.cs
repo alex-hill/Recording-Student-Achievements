bfd8db4c23023f3c452f4e9f46316816c65a142d
PunkyMonk <t43p4rk@gmail.com>
2016-05-19 00:13:31 +1200

    Started a View All data table on a panel that can be changed Also changed add a student's textboxs to a few combo boxes

--- Recording Student Achievements/Recording Student Achievements/Properties/Resources.Designer.cs
+++ Recording Student Achievements/Recording Student Achievements/Properties/Resources.Designer.cs
@@ -93,6 +93,36 @@ namespace Recording_Student_Achievements.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Large_Box {
+            get {
+                object obj = ResourceManager.GetObject("Large Box", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo {
+            get {
+                object obj = ResourceManager.GetObject("Logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Logo1 {
+            get {
+                object obj = ResourceManager.GetObject("Logo1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Quick_Menu {
             get {
                 object obj = ResourceManager.GetObject("Quick Menu", resourceCulture);
