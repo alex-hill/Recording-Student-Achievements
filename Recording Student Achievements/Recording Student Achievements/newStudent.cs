e029377242c09f67da232d833a3174a4e5fde3ce
alex-hill <rts8039@autuni.ac.nz>
2016-08-01 16:55:25 +1200

    changed add and remove students to excel file

--- Recording Student Achievements/Recording Student Achievements/newStudent.cs
+++ Recording Student Achievements/Recording Student Achievements/newStudent.cs
@@ -17,7 +17,7 @@ namespace Recording_Student_Achievements
         public NewStudent()
         {
             InitializeComponent();
-            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Table.accdb;Persist Security Info=False;"; //For not Alex's laptop
+            conn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Database.xlsx;Persist Security Info=False;Extended Properties=Excel 12.0;"; //For not Alex's laptop
             //conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=|DataDirectory|\\Table.mdb;Persist Security Info=True"; //For Alex's laptop
         }
 
@@ -34,7 +34,7 @@ namespace Recording_Student_Achievements
         private void button1_Click(object sender, EventArgs e)
         {
 
-              OleDbCommand cmd = new OleDbCommand("INSERT INTO Student ([Family Name Alias], [Family Name Legal], [First Name Legal], [Preferred Name], [Year Level], [Room Number], [Gender], [Date of Birth], [Ethnicity], [NSN], [Funding Year Level], [Start Date])"
+              OleDbCommand cmd = new OleDbCommand("INSERT INTO [Sheet1$] ([Family Name Alias], [Family Name Legal], [First Name Legal], [Preferred Name], [Year Level], [Room Number], [Gender], [Date of Birth], [Ethnicity], [NSN], [Funding Year Level], [Start Date])"
                   +" VALUES(" + familyNameAlias.Text + ", " + familyNameLegal.Text + ", [" + firstNameLegal.Text + "], " + preferredName.Text + ", " + yearLevelCombo.Text + ", " + roomCombo.Text
                    + ", " + genderCombo.Text + ", " + dateOfBirth.Text + ", " + ethnicityCombo.Text + ", " + nsn.Text + ", " + fundingLevelCombo.Text + ", " + startDate.Text + ");");
 
