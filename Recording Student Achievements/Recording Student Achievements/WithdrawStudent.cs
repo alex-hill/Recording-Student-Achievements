e1d3c92b5cd7b4bc15e6196c8dba2d63751cd2b9
alex-hill <rts8039@autuni.ac.nz>
2016-08-27 19:13:24 +1200

    Changed withdraw student to remove student from all relevant tables

--- Recording Student Achievements/Recording Student Achievements/WithdrawStudent.cs
+++ Recording Student Achievements/Recording Student Achievements/WithdrawStudent.cs
@@ -22,9 +22,20 @@ namespace Recording_Student_Achievements
 
         private void removeOneStudent_click(object sender, EventArgs e)
         {
+            OleDbCommand nsn = new OleDbCommand("SELECT NSN FROM Student WHERE [First Name Legal] LIKE '%" + legalNameTxtBox.Text + "%' AND [Famile Name Legal] LIKE '%" + familyNameTxtBox.Text + "%';");
+            nsn.Connection = conn;
+            OleDbDataReader reader = nsn.ExecuteReader();
+            string nssn = reader.GetString(0);
 
             OleDbCommand cmd = new OleDbCommand("DELETE FROM Student WHERE [First Name Legal] LIKE '%" + legalNameTxtBox.Text + "%' AND [Family Name Legal] LIKE '%" + familyNameTxtBox.Text + "%'");
-
+            OleDbCommand cmd1 = new OleDbCommand("DELETE FROM Calculated WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd2 = new OleDbCommand("DELETE FROM [Student Extra] WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd3 = new OleDbCommand("DELETE FROM Reading WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd4 = new OleDbCommand("DELETE FROM Writing WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd5 = new OleDbCommand("DELETE FROM Mathematics WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd6 = new OleDbCommand("DELETE FROM [Cultural Activities] WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd7 = new OleDbCommand("DELETE FROM [Extra Activities] WHERE [NSN] LIKE '%" + nssn + "'%");
+            OleDbCommand cmd8 = new OleDbCommand("DELETE FROM [Sports Activities] WHERE [NSN] LIKE '%" + nssn + "'%");
             // OleDbCommand cmd = new OleDbCommand("INSERT INTO Student (Gender, NSN) VALUES ('" + textBox7.Text + "', '" + textBox10.Text + "');");
             cmd.Connection = conn;
 
@@ -36,6 +47,14 @@ namespace Recording_Student_Achievements
                 try
                 {
                     cmd.ExecuteNonQuery();
+                    cmd1.ExecuteNonQuery();
+                    cmd2.ExecuteNonQuery();
+                    cmd3.ExecuteNonQuery();
+                    cmd4.ExecuteNonQuery();
+                    cmd5.ExecuteNonQuery();
+                    cmd6.ExecuteNonQuery();
+                    cmd7.ExecuteNonQuery();
+                    cmd8.ExecuteNonQuery();
                     MessageBox.Show("Data Removed");
                     conn.Close();
                 }
