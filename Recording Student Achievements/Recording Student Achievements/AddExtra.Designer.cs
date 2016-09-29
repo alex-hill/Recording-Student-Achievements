79280f6104ceed9bc11f2ab2058764364015c64e
alex-hill <rts8039@autuni.ac.nz>
2016-09-29 21:43:13 +1300

    Done

--- Recording Student Achievements/Recording Student Achievements/AddExtra.Designer.cs
+++ Recording Student Achievements/Recording Student Achievements/AddExtra.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddExtra));
             this.addStudentExtra = new System.Windows.Forms.Button();
             this.updateExtra = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // addStudentExtra
@@ -53,17 +54,29 @@
             this.updateExtra.UseVisualStyleBackColor = true;
             this.updateExtra.Click += new System.EventHandler(this.updateExtra_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 144);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
             // AddExtra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.updateExtra);
             this.Controls.Add(this.addStudentExtra);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "AddExtra";
             this.Text = "Add Student Data";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -71,5 +84,6 @@
 
         private System.Windows.Forms.Button addStudentExtra;
         private System.Windows.Forms.Button updateExtra;
+        private System.Windows.Forms.Label label1;
     }
 }
