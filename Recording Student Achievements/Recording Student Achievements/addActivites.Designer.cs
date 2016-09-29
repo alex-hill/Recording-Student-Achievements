d5973e2c2118111a8553d701a72495d901a87b90
alex-hill <rts8039@autuni.ac.nz>
2016-09-29 15:37:44 +1300

    Activities adding and updating completed

--- Recording Student Achievements/Recording Student Achievements/addActivites.Designer.cs
+++ Recording Student Achievements/Recording Student Achievements/addActivites.Designer.cs
@@ -29,23 +29,35 @@
         private void InitializeComponent()
         {
             this.addActivities = new System.Windows.Forms.Button();
+            this.updateActivities = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // addActivities
             // 
             this.addActivities.Location = new System.Drawing.Point(13, 227);
             this.addActivities.Name = "addActivities";
-            this.addActivities.Size = new System.Drawing.Size(102, 23);
+            this.addActivities.Size = new System.Drawing.Size(126, 23);
             this.addActivities.TabIndex = 0;
             this.addActivities.Text = "Add Activites";
             this.addActivities.UseVisualStyleBackColor = true;
             this.addActivities.Click += new System.EventHandler(this.addActivities_Click);
+            // 
+            // updateActivities
+            // 
+            this.updateActivities.Location = new System.Drawing.Point(146, 227);
+            this.updateActivities.Name = "updateActivities";
+            this.updateActivities.Size = new System.Drawing.Size(126, 23);
+            this.updateActivities.TabIndex = 1;
+            this.updateActivities.Text = "Update Activities";
+            this.updateActivities.UseVisualStyleBackColor = true;
+            this.updateActivities.Click += new System.EventHandler(this.updateActivities_Click);
             // 
             // addActivites
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.updateActivities);
             this.Controls.Add(this.addActivities);
             this.Name = "addActivites";
             this.Text = "Add Activites";
@@ -56,5 +68,6 @@
         #endregion
 
         private System.Windows.Forms.Button addActivities;
+        private System.Windows.Forms.Button updateActivities;
     }
 }
